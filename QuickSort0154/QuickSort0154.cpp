081eff6d712aa2c016cc1b755380613a2d22bcbb
Cyberfire <buayagoreng99@gmail.com>
2024-03-28 11:06:21 +0800

    membuat fungsi untuk menyimpan nilai

--- QuickSort0154/QuickSort0154.cpp
+++ QuickSort0154/QuickSort0154.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 
+// array of imtegers to hold values
+int arr[20];
+int cmp_count = 0; // number of comparasion
+int mov_count = 0; // number of data movements
+int n;
+
 int main()
 {
 
