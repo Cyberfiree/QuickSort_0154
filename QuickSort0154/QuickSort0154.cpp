db49b7e040bd5562266ac27d96510d295770359a
Cyberfire <buayagoreng99@gmail.com>
2024-03-28 11:53:06 +0800

    membuat if untuk menukar element pada index j

--- QuickSort0154/QuickSort0154.cpp
+++ QuickSort0154/QuickSort0154.cpp
@@ -59,6 +59,10 @@ void q_short(int low, int high) {
             cmp_count++;
         }
         cmp_count++;
+        if (i < j) { // step 9
+            // swap the pivot element with the element at index j
+            swap(low, j);
+        }
     }
 }
 
