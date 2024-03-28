3695f445d916f8a372171b42508e9e5517f4f905
Cyberfire <buayagoreng99@gmail.com>
2024-03-28 11:24:27 +0800

    membuat tampilan perintah masukkan array

--- QuickSort0154/QuickSort0154.cpp
+++ QuickSort0154/QuickSort0154.cpp
@@ -16,6 +16,10 @@ void input() {
             break;
         else
             cout << "\nMaksimum panjang array adalah 20" << endl;
+
+        cout << "\n-------------------" << endl;
+        cout << "\nEnter Array Element" << endl;
+        cout << "\n-------------------" << endl;
     }
 }
 
